29536fc86aa812a84e1d88b3cf456c9bccd70e02
Alex Bannow <rbannal@gmail.com>
2019-09-25 15:27:20 -0400

    added roughly correct css

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const store = {
   mealCounter: 0, tipCounter: 0, subTotal: 0, tipAmount: 0, totalAmount: 0, avgTip: 0
 };
@@ -7,22 +9,22 @@ const store = {
 //renders html for MY EARNINGS INFO html
 const renderMyEarningsInfo = function() {
   const myEarningsString = `<h2>MY EARNINGS INFO</h2>
-  <p>
+  <p class="earningBox">
     TIP TOTAL: ${store.tipCounter.toFixed(2)} <br>
     MEAL COUNT: ${store.mealCounter} <br>
     TIP AVERAGE: ${store.avgTip.toFixed(2)}
-  </p>`
+  </p>`;
   $('.js-earnings-info').html(myEarningsString);
 };
 
 //renders html for CUSTOMER CHARGES html
 const renderCustomerCharges = function() {
   const customerChargesString = `<h2>CUSTOMER CHARGES</h2>
-  <p>
+  <p class="chargesBox">
     SUBTOTAL: ${store.subTotal.toFixed(2)} <br>
     TIP: ${store.tipAmount.toFixed(2)} <br>
     TOTAL: ${store.totalAmount.toFixed(2)}
-  </p>`
+  </p>`;
   $('.js-customer-charges').html(customerChargesString);
 };
 
@@ -41,24 +43,23 @@ const valCalc = function(mealPrice, taxRate, tipRate) {
 
 //gathers numbers from meal info form
 const submitMealNumbers = function() {
-    $('.js-meal-input').submit(function(event) {
-      event.preventDefault();
-      store.tipAmount = 0;
-      store.subTotal = 0;
-      store.totalAmount = 0;
-      console.log("CLICK");
-      const mealPrice = $('.js-meal-price').val();
-      const taxRate = $('.js-tax-rate').val();
-      const tipRate = $('.js-tip-rate').val();
-      $('.js-meal-price').val('');
-      $('.js-tax-rate').val('');
-      $('.js-tip-rate').val('');
-      store.mealCounter++;
-      valCalc(mealPrice, taxRate, tipRate);
-      renderCustomerCharges();
-      renderMyEarningsInfo();
-    });
-  };
+  $('.js-meal-input').submit(function(event) {
+    event.preventDefault();
+    store.tipAmount = 0;
+    store.subTotal = 0;
+    store.totalAmount = 0;
+    const mealPrice = $('.js-meal-price').val();
+    const taxRate = $('.js-tax-rate').val();
+    const tipRate = $('.js-tip-rate').val();
+    $('.js-meal-price').val('');
+    $('.js-tax-rate').val('');
+    $('.js-tip-rate').val('');
+    store.mealCounter++;
+    valCalc(mealPrice, taxRate, tipRate);
+    renderCustomerCharges();
+    renderMyEarningsInfo();
+  });
+};
 
 
 //resets values in form when clicking cancel
@@ -73,7 +74,7 @@ const handleCancelClick = function() {
 
 //listens for reset button
 const resetButton = function() {
-  $('.js-reset-button').click(function(event) {
+  $('.js-reset-button').click(function() {
     location.reload();
   });
 };
